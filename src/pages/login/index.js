16c82d513f319983d5fc9f82b395604f4a3a7ddb
Inteligencia <inteligencia.cicopal@gmail.com>
2026-02-06 11:06:48 -0300

    initial commits

---
+++ src/pages/login/index.js
@@ -0,0 +1,26 @@
+export async function signin(dados) {
+    const url = "https://intcicopal.github.io/rota/db/users.json";
+
+    try {
+        const response = await fetch(url);
+        const usuarios = await response.json();
+
+        const usuarioEncontrado = usuarios.find(u =>
+            u.user.toLowerCase() === dados.user.toLowerCase()
+        );
+
+        if (!usuarioEncontrado) {
+            throw new Error("Credenciais invalidas");
+        }
+
+        if (usuarioEncontrado.password.toString() !== dados.password) {
+            throw new Error("Credenciais invalidas");
+        }
+
+        sessionStorage.setItem("usuario", JSON.stringify(usuarioEncontrado));
+        window.location = "/"
+
+    } catch (erro) {
+        console.error("Erro ao buscar usuários:", erro);
+    }
+}
---
+++ src/shared/guard.js
@@ -0,0 +1,15 @@
+(function () {
+    const usuario = sessionStorage.getItem("usuario");
+
+    try {
+        if (!usuario) throw new Error("Sem sessão");
+
+        const dados = JSON.parse(usuario);
+
+        // se quiser validar campo obrigatório
+        if (!dados || !dados.user) throw new Error("Sessão inválida");
+
+    } catch (e) {
+        window.location.href = "src/pages/login/";
+    }
+})();
